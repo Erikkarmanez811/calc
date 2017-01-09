854d4ad2fc1ab81e635279e75293a8bb4b9590b8
Alexander Avdoshkin <alexavdoshkin@gmail.com>
2017-01-09 21:59:18 +0300

    Complete program

--- calc.py
+++ calc.py
@@ -7,7 +7,7 @@ def lex(AS):
     token1 = ''
     aS = list(AS)
     for i in aS:
-        if i in operations:
+        if i in operations + brackets:
             
             if token != []:
                 for q in token:
@@ -33,7 +33,7 @@ def lex(AS):
 
 
 def prior(oper):
-    opers = '()+-*/'
+    opers = '+-*/()'
     if oper in opers:
         return opers.index(oper)
     print('Error')
@@ -43,14 +43,13 @@ def prior(oper):
 def rpn(exp):
     rpn = []
     stack = []
-    
     for token in exp:
         if type(token) == int:
             rpn.append(token)
         
 
 
-    if token in brackets:
+        if token in brackets:
             if token == '(':
                 stack.append(token)
             if token == ')':
@@ -59,64 +58,61 @@ def rpn(exp):
                     rpn.append(sap)
                     sap = stack.pop()
             
-    if token in operations:
+        if token in operations:
             if stack == []:
                 stack.append(token)
             else:
-                op1 = token
+
+                
                 op2 = stack.pop()
 
-
-                while prior(op2) >= prior(op1):
+                while prior(op2) <= prior(token):
                     rpn.append(op2)
-                    op2 = stack.pop()
+                    if len(stack):
+                        op2 = stack.pop()
+                    else:
+                            break
 
 
                     
-                if prior(op1) <= prior(op2):
-                    while prior(op2) >= prior(op1):
-                        op2 = stack.pop()
-                        opz.append(op2)
-                        if stack == []:
-                           stack.append(token)
-                           break
-                        else:
-                            if prior(op1) > prior(op2):
-                                stack.append(token)
-                                break
+                if prior(op2) > prior(token):
+                    stack.append(op2)
 
                 stack.append(token)
+  
+    while len(stack):
+        rpn.append(stack.pop())
+    return rpn
+            
 
-                            
-
-                    
-                        
-                        
-                    
-        if token == '^':
-            stack.reverse()
-            for qwe in stack:
-                opz.append(qwe)
-            while '(' in opz:
-                opz.remove('(')
-            print (opz)    
+def evaluate(rpn_exp):
+    stack = []
+    for elem in rpn_exp:
+        if type(elem) == int:
+            stack.append(elem)
+        if elem == '+':
+            stack.append(stack.pop()+stack.pop())
+        if elem == '-':
+            stack.append(-stack.pop()+stack.pop())
+        if elem == '*':
+            stack.append(stack.pop()*stack.pop())
+        if elem == '/':
+            stack.append(1/(stack.pop()/stack.pop()))
+    return stack.pop()
 
 
         
 operations = ['+' , '-'  , '*' , '/' ]
 brackets = ['(',')']
 
-print(prior('*'))
 while True:
     exp = str(input())
     
     exp_l = lex(exp)
-    print(exp_l)
-    exp_l.append('^')
-    op1 = ''
-    op2 = ''
-    opp1 = 0
-    opp2 = 0
+    
+    rpn_exp = rpn(exp_l)
+
+    print(evaluate(rpn_exp), 'result')
    
             
         
