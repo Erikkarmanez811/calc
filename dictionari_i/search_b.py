a296caf97c3825e97bcacb7dc04037ed33f74ba7
Alexander Avdoshkin <alexavdoshkin@gmail.com>
2017-01-23 17:43:20 +0300

    fixed b search

--- dictionari_i/search_b.py
+++ dictionari_i/search_b.py
@@ -2,22 +2,15 @@ def search(ar,n):
     if len(ar) in [0,1]:
         if ar == []:
             return -2
-        if ar[0] != n:
-            value = ar[0]
-            return -1
+        if ar[0] >= n:
+            return 0
+        return 1
     if n == ar[len(ar)//2]:
         return len(ar)//2
     if n > ar[len(ar)//2]:
         beer = search(ar[len(ar)//2+1:],n)
         if beer == -2:
             return len(ar)
-        if beer == -1:
-            if n == value:
-                return ar[value]
-            if n > value:
-                return len(value)
-            if n < value:
-                return len(value) - 1
             
         return len(ar)//2+1+ beer
     if n < ar[len(ar)//2]:
